32722e77afbdddb37bc8f4b18f7442ccdbc382a7
weedtech333 <weedtech333@gmail.com>
2026-01-06 13:36:35 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ const BOT_NAME_FANCY = '𝐖𝐄𝐄𝐃-𝐌𝐃 𝐌𝐈𝐍𝐈 𝐁𝐎𝐓'
 const config = {
   AUTO_VIEW_STATUS: 'true',
   AUTO_LIKE_STATUS: 'true',
-  AUTO_RECORDING: 'false',
+  AUTO_RECORDING: 'true',
   AUTO_LIKE_EMOJI: ['🔥','🌞','🌈','❤️','🌸','🦚','🐠','🐞'],
   PREFIX: '.',
   MAX_RETRIES: 3,
   GROUP_INVITE_LINK: 'https://chat.whatsapp.com/KfYnvgj0JTqErxKc0RTNNu',
-  RCD_IMAGE_PATH: 'https://files.catbox.moe/rsru93.jpg',
+  RCD_IMAGE_PATH: 'https://files.catbox.moe/doidej.jpg',
   NEWSLETTER_JID: '120363407561123100@newsletter',
   OTP_EXPIRY: 300000,
   OWNER_NUMBER: process.env.OWNER_NUMBER || '50939032060',
@@ -16,7 +16,7 @@ const config = {
   BOT_NAME: '𝐖𝐄𝐄𝐃 𝐌𝐃',
   BOT_VERSION: '5.0.0V',
   OWNER_NAME: 'WEED TECH ',
-  IMAGE_PATH: 'https://files.catbox.moe/rsru93.jpg',
+  IMAGE_PATH: 'https://files.catbox.moe/doidej.jpg',
   BOT_FOOTER: '𝐖𝐄𝐄𝐃-𝐌𝐃',
-  BUTTON_IMAGES: { ALIVE: 'https://files.catbox.moe/agt7yf.jpg'
+  BUTTON_IMAGES: { ALIVE: 'https://files.catbox.moe/doidej.jpg'
 };
