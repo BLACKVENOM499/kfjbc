7ef425bab723073a840cd035ce50fdc39566a683
weedtech333 <weedtech333@gmail.com>
2026-01-06 12:45:46 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const BOT_NAME_FANCY = '𝐑𝐀𝐕𝐈𝐘𝐀-𝐌𝐃 𝐌𝐈𝐍𝐈 𝐁𝐎𝐓';
+const BOT_NAME_FANCY = '𝐖𝐄𝐄𝐃-𝐌𝐃 𝐌𝐈𝐍𝐈 𝐁𝐎𝐓';
 
 const config = {
   AUTO_VIEW_STATUS: 'true',
@@ -7,16 +7,16 @@ const config = {
   AUTO_LIKE_EMOJI: ['🔥','🌞','🌈','❤️','🌸','🦚','🐠','🐞'],
   PREFIX: '.',
   MAX_RETRIES: 3,
-  GROUP_INVITE_LINK: 'https://chat.whatsapp.com/Eqv2HOSQt1TGvB4NHtmDjL?mode=hqrt2',
-  RCD_IMAGE_PATH: 'https://i.ibb.co/gM997gRp/tourl-1765543281755.jpg',
-  NEWSLETTER_JID: '120363401853152721@newsletter',
+  GROUP_INVITE_LINK: 'https://chat.whatsapp.com/KfYnvgj0JTqErxKc0RTNNu',
+  RCD_IMAGE_PATH: 'https://files.catbox.moe/rsru93.jpg',
+  NEWSLETTER_JID: '120363407561123100@newsletter',
   OTP_EXPIRY: 300000,
-  OWNER_NUMBER: process.env.OWNER_NUMBER || '94785505762',
-  CHANNEL_LINK: 'https://whatsapp.com/channel/0029VbB8Sv72phHMubLEgH0F',
-  BOT_NAME: '𝐑𝐀𝐕𝐈𝐘𝐀 𝐌𝐃 𝐕1',
+  OWNER_NUMBER: process.env.OWNER_NUMBER || '50939032060',
+  CHANNEL_LINK: 'https://whatsapp.com/channel/0029Vb2J9C91dAw7vxA75y2V',
+  BOT_NAME: '𝐖𝐄𝐄𝐃 𝐌𝐃',
   BOT_VERSION: '5.0.0V',
-  OWNER_NAME: 'PAMUDINA RAVIHARA ',
-  IMAGE_PATH: 'https://i.ibb.co/KxrG6Fpm/IMG-20251011-WA0084.jpg',
-  BOT_FOOTER: '𝐑𝐀𝐕𝐈𝐘𝐀-𝐌𝐃 𝐕1',
-  BUTTON_IMAGES: { ALIVE: 'https://i.ibb.co/gM997gRp/tourl-1765543281755.jpg'
+  OWNER_NAME: 'WEED TECH ',
+  IMAGE_PATH: 'https://files.catbox.moe/rsru93.jpg',
+  BOT_FOOTER: '𝐖𝐄𝐄𝐃-𝐌𝐃',
+  BUTTON_IMAGES: { ALIVE: 'https://files.catbox.moe/agt7yf.jpg'
 };
